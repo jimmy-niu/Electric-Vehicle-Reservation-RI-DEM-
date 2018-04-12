29777cb10019569c3577baccf839fc3db7dadfb0
jtishler <jenna@tishler.net>
2018-04-12 00:35:00 -0400

    set up node and simple server.js file

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var app = express();
+var path = require('path');
+var http =require('http');
+var server = http.createServer(app);
+
+var io = require('socket.io')(server, {wsEngine: 'ws'}); //fix Windows10 issue
+//TO-DO change before handin!
+io.listen(server); 
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+var anyDB = require('any-db');
+//var conn = anyDB.createConnection('sqlite3://chatroom.db');
+
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.get('/', function(request, response){
+	console.log('- Request received:', request.method, request.url);
+
+	response.send("hello, world");
+});
+
+/*Sets up the server on port 8080.*/
+server.listen(8080, function(){
+	console.log('- Server listening on port 8080');
+});
+
+//handles events when a user is connected
+io.sockets.on('connection', function(socket){
+   
+
+});
+
+
