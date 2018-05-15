8522235e7a48e5d7dbfdea865c1103d48067f650
Jimmy Niu <chenenniu@gmail.com>
2018-05-15 04:05:51 -0400

    editReservation on admin side. reassignVehicle is not working

--- public/admin/scripts/reservation.js
+++ public/admin/scripts/reservation.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
 
     adminSocket.on('loadReservations', function(reservations){
         $('#upcoming').empty();
-        console.log(reservations);
         for(let i = 0; i < reservations.rowCount; i++){
             new Reservation(reservations.rows[i]);
         }
@@ -58,13 +57,40 @@ function getJustificationModal(id, text){
 }
 
 function editReservation(res_data){
+    console.log("in edit reservation data");
     console.log(res_data);
+    
+}
+
+function getReservationDom(reservationData){
+    let justification_button = "";
+    let modal = "";
+
+    if(r.justification !== '' && r.justification !== null && r.justification !== undefined){
+        modal = getJustificationModal(r.id, r.justification);
+
+        justification_button = `<a href = "#justification_modal_${r.id}" class = "btn btn-large btn-primary drop-shadow" data-toggle="modal">Click To See</a>`;
+    }
+
+
+    //console.log(r);
+    let DOMobject = `<div class = "col-entry reservation-id res_id_${r.id}">${r.id}</div>`
+    +`<div class = "col-entry reservation-user res_id_${r.id}">${r.user}</div>`
+    + `<div class = "col-entry reservation-start res_id_${r.id}">${r.start}</div>`
+    + `<div class = "col-entry reservation-end res_id_${r.id}">${r.end}</div>`
+    + `<div class = "col-entry carModel res_id_${r.id}">${r.model}</div>`
+    + `<div class = "col-entry reservation-license res_id_${r.id}">${r.license}</div>`
+    + `<div class = "col-entry reservation-pickup> res_id_${r.id}">${justification_button}</div>`
+    + modal;
+    
+    return DOMobject;
 }
 
 class Reservation {
     constructor(reservationData){
         this.addToDOM(reservationData);
     }
+    
     addToDOM(r){
         let justification_button = "";
         let modal = "";
@@ -85,7 +111,7 @@ class Reservation {
         + `<div class = "col-entry reservation-license res_id_${r.id}">${r.license}</div>`
         + `<div class = "col-entry reservation-pickup> res_id_${r.id}">${justification_button}</div>`
         + modal;
-
+    
         $('#upcoming').append(DOMobject);
     }
 }
