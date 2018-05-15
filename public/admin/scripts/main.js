4468de8ba108947f911a16657a8eeb6d923bf551
Jimmy Niu <chenenniu@gmail.com>
2018-05-14 22:03:07 -0400

    Fixed reservation id in reports table

--- public/admin/scripts/main.js
+++ public/admin/scripts/main.js
@@ -39,7 +39,6 @@ $(document).ready(function() {
 
     adminSocket.on('reportChange', function(reports){
         console.log("we are in reportchange!");
-        console.log(reports);
 
         $('#reports').empty();
         for(let i = 0; i < reports.rowCount; i++){
@@ -307,14 +306,15 @@ class Reservation {
 
 
         //console.log(r);
-        let DOMobject = `<div class = "col-entry reservation-user ${r.license}">${r.user}</div>`
+        let DOMobject = `<div class = "col-entry reservation-id ${r.license}">${r.id}</div>`
+        +`<div class = "col-entry reservation-user ${r.license}">${r.user}</div>`
         + `<div class = "col-entry reservation-start ${r.license}">${r.start}</div>`
         + `<div class = "col-entry reservation-end ${r.license}">${r.end}</div>`
         + `<div class = "col-entry carModel ${r.license}">${r.model}</div>`
         + `<div class = "col-entry reservation-license ${r.license}">${r.license}</div>`
         + `<div class = "col-entry reservation-pickup> ${r.license}">${justification_button}</div>`
         + modal;
-        
+
         $('#upcoming').append(DOMobject);
     }
 }
@@ -346,10 +346,11 @@ class Vehicle {
 class Report {
     constructor(reportData){
         this.addToDOM(reportData);
+        console.log(reportData);
     }
 
     addToDOM(r){
-        let DOMobject = `<div class = "col-entry report-res-id ${r.id}">${r.id}</div>`
+        let DOMobject = `<div class = "col-entry report-res-id ${r.id}">${r.reservation}</div>`
         + `<div class = "col-entry report-content ${r.id}">${r.report}</div>`
         + `<div class = "col-entry needs-cleaning ${r.id}">${getBooleanStr(r.needsCleaning)}</div>`
         + `<div class = "col-entry needs-service ${r.id}">${getBooleanStr(r.needsService)}</div>`
